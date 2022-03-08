33a072074cbc4ac92b22b65abf51e371d11898e7
Chris Kelley <chriskelley85@gmail.com>
2022-03-08 14:24:50 -0600

    server & get post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,15 @@
 // implement your posts router here
+const Posts = require('./posts-model');
+const express = require('express');
+const router = express.Router()
+
+router.get('/', async(req,res)=> {
+    try{
+        const post = await Posts.find()
+        res.json(post)
+    }
+    catch{
+        res.status(500).json({ message: "The post is unavailable" })
+    }
+})
+module.exports = router;
